6d68f7ae876999a032672f1e414b0681b1c052f7
Evgeny Pavlov <lucenticus@gmail.com>
2023-03-02 00:52:42 +0200

    Add more headers for bindings generation

--- wrapper.h
+++ wrapper.h
@@ -3,5 +3,6 @@
 #include <d3d9.h>
 #include <d3d11.h>
 #include <process.h>
-
+#include <windows.h>
+#include "./AMF/amf/public/include/core/Factory.h"
 #include "./AMF/amf/public/include/components/VideoEncoderVCE.h"
