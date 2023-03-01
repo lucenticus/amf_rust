fe6b74494f92235ae613d652591e36cf919c618b
Evgeny Pavlov <lucenticus@gmail.com>
2023-03-02 00:33:42 +0200

    Added project-related files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # amf_rust
+
+This repo contains example of using AMD AMF library with Rust language.  
---
+++ wrapper.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <d3d9.h>
+#include <d3d11.h>
+#include <process.h>
+
+#include "./AMF/amf/public/include/components/VideoEncoderVCE.h"
